d0f9f2d0a74b6364d32bfb71a98d7cc647b52f00
syedkhizar123 <khizarrehman2007@gmail.com>
2024-09-14 14:55:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,174 @@
+document.write("CHAPTER: 1 <br>")
+alert('Error! Please enter a valid password')
+alert('Welcome to Js Land... \n Happy Coding!')
+
+alert('Welcome to JS Land...')
+alert('Happy Coding!')
+
+document.write("Hello... I can run JS through my web browser's console <br><br><br><br>")
+
+
+
+
+
+
+document.write("CHAPTER: 2 <br>")
+var username 
+var myname 
+myname = 'Khizar Rehman'
+var message = 'Hello World'
+alert(message)
+
+var student_name = 'John Doe'
+var student_age = '15'
+var student_course = 'Certified Mobile Application Development'
+alert(student_name)
+alert(student_age + " years old. ")
+alert(student_course)
+
+var pizza = ' PIZZA \n PIZZ \n PIZ \n PI \n P'
+alert(pizza)
+
+var email = 'muqaddam@gmail.com'
+alert("My email address is " + email)
+
+var book = 'A smarter way to learn JavaScript'
+alert("I am trying to learn from the book " + book)
+
+document.write("Yah! I can write HTML content through JavaScript <br><br><br><br>")
+
+var unique = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(unique)
+
+
+
+
+
+
+document.write("CHAPTER: 3 <br>")
+
+var age = 16
+alert("I am " + age +" years old.")
+
+var no_of_visits = 17
+alert("You have visited this site "+no_of_visits+" times.")
+
+var birth_year = 2007
+document.write("My birth year is " + birth_year + "<br>Data type of declared variable is number.<br>")
+
+var visitor_name = "John Doe"
+var product = "T-Shirts"
+var quantity = "5 "
+document.write(visitor_name + " ordered " + quantity + product + " from XYZ clothing store. <br><br><br><br>")
+
+
+
+
+
+
+
+
+
+document.write("CHAPTER: 5 <br>")
+
+var first_number = +prompt("Enter first number: ")
+var second_number = +prompt("Enter second number: ")
+document.write("The sum of " + first_number + " and " + second_number + " is " + Number(first_number + second_number) + "<br>")
+document.write("The difference of " + first_number + " and " + second_number + " is " + Number(first_number - second_number) + "<br>")
+document.write("The product of " + first_number + " and " + second_number + " is " + Number(first_number * second_number) + "<br>")
+document.write("The division of " + first_number + " and " + second_number + " is " + Number(first_number / second_number) + "<br>")
+document.write("The modulus of " + first_number + " and " + second_number + " is " + Number(first_number % second_number) + "<br>")
+
+var new_variable
+document.write("Value after variable declaration is " + new_variable + "<br>")
+new_variable = 20
+document.write("Initial value: " + new_variable + "<br>")
+document.write("Value after increment is " + ++new_variable + "<br>")
+new_variable = new_variable + 7
+document.write("Value after addition is " + new_variable + "<br>")
+document.write("Value after decrement is " + --new_variable + "<br>")
+document.write("The remainder is " + new_variable%3 + "<br>")
+
+
+var tkt_price = 600
+var no_of_tkts = 5
+document.write("The cost to buy " + no_of_tkts + " movie tickets is " + Number(no_of_tkts*tkt_price) + "PKR <br>" )
+
+var table = 10
+document.write("Table of " + table + "<br>")
+document.write(table + " x 1 = " + Number(table*1) + "<br>")
+document.write(table + " x 2 = " + Number(table*2) + "<br>")
+document.write(table + " x 3 = " + Number(table*3) + "<br>")
+document.write(table + " x 4 = " + Number(table*4) + "<br>")
+document.write(table + " x 5 = " + Number(table*5) + "<br>")
+document.write(table + " x 6 = " + Number(table*6) + "<br>")
+document.write(table + " x 7 = " + Number(table*7) + "<br>")
+document.write(table + " x 8 = " + Number(table*8) + "<br>")
+document.write(table + " x 9 = " + Number(table*9) + "<br>")
+document.write(table + " x 10 = " + Number(table*10) + "<br>")
+
+var C_temp = 25
+var F_temp = (C_temp * 9/5) + 32
+var f_temp = 70
+var c_temp = (f_temp - 32) * 5/9
+document.write(f_temp+ "F in Celsius is " + c_temp + "<br>")
+document.write(C_temp+ "C in Fehrenheight is " + F_temp + "<br>")
+
+
+var prc_item1 = 700
+var prc_item2 = 1500
+var qty_item1 = 5
+var qty_item2 = 2
+var shipping_charges = 1000
+
+document.write("Price of item 1 is " + prc_item1 + "<br>")
+document.write("Quantity of item 1 is " + qty_item1 + "<br>")
+document.write("Price of item 2 is " + prc_item2 + "<br>")
+document.write("Quantity of item 2 is " + qty_item2 + "<br>")
+document.write("Shipping Charges " + shipping_charges + "<br>")
+document.write("Total cost of your order is " + Number((prc_item1*qty_item1)+(prc_item2*qty_item2)+shipping_charges) + "<br>")
+
+
+
+var total_marks = 980
+var obtained_marks = 804
+var perc = (obtained_marks/total_marks)*100
+document.write("Total Marks: " + total_marks + "<br>")
+document.write("Marks Obtained: " + obtained_marks + "<br>")
+document.write("Percentage: " + perc + "<br>")
+
+var usd = 10
+var sdi_ryl = 25
+var pkr = (usd*280) + (sdi_ryl*75)
+document.write("USD: " + usd + "<br>")
+document.write("SAUDI RIYAL: " + sdi_ryl + "<br>")
+document.write("Total amount in PKR: " + pkr + "<br>")
+
+var q10 = 10
+var q10_operations = ((q10 + 5)*10)/12
+document.write("After performing the given operations on " + q10 + " we get " + q10_operations + "<br>")
+
+var crnt_year = 2024
+var brth_year = +prompt("Enter your birth year: ")
+var age1 = crnt_year - brth_year
+var age2 = crnt_year - brth_year - 1
+document.write("You are either " + age2 + " or " + age1 + " years old.<br>")
+
+
+var radius = 20
+var circumference = 2*3.142*radius
+var area = (3.142)*(radius*radius)
+document.write("Radius of a circle: " + radius + "<br>")
+document.write("Circumference of a circle: " + circumference + "<br>")
+document.write("Area of a circle: " + area + "<br>")
+
+var fav_snack = "chocolate chip"
+var crnt_age = 16
+var mxm_age = 80
+var avg_snks_per_day = 2
+var final_ans = ((mxm_age-crnt_age)*365)*2
+document.write("Favourite snack: " + fav_snack + "<br>")
+document.write("Current Age: " + crnt_age + "<br>")
+document.write("Estimated Maximum Age: " + mxm_age + "<br>")
+document.write("Amount of snacks per day: " + avg_snks_per_day + "<br>")
+document.write("You will need " + final_ans + " " + fav_snack + " to last you until the ripe old age of "+ mxm_age+ "<br>")
